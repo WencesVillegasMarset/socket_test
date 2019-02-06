b145036a4586c120ffb0d54504b851a149f04f78
WencesVillegasMarset <villegaswences@gmail.com>
2019-02-06 01:09:49 -0300

    Agregada script multisocket

--- multi-conn-server.py
+++ multi-conn-server.py
@@ -1,8 +1,43 @@
+import socket
 import selectors
+import types
+
+def accept_wrapper(sock):
+    conn, addr = sock.accept()  # Should be ready to read
+    print('accepted connection from', addr)
+    conn.setblocking(False)
+    data = types.SimpleNamespace(addr=addr, inb=b'', outb=b'')
+    events = selectors.EVENT_READ | selectors.EVENT_WRITE
+    sel.register(conn, events, data=data)
+
+def service_connection(key, mask):
+    sock = key.fileobj
+    data = key.data
+    if mask & selectors.EVENT_READ:
+        recv_data = sock.recv(1024)  # Should be ready to read
+        if recv_data:
+            data.outb += recv_data
+        else:
+            print('closing connection to', data.addr)
+            sel.unregister(sock)
+            sock.close()
+            print(data.outb)
+
+HOST = '192.168.0.32'  # Standard loopback interface address (localhost)
+PORT = 8880        # Port to listen on (non-privileged ports are > 1023)
+
 sel = selectors.DefaultSelector()
 lsock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-lsock.bind((host, port))
+lsock.bind((HOST, PORT))
 lsock.listen()
-print('listening on', (host, port))
+print('listening on', (HOST, PORT))
 lsock.setblocking(False)
 sel.register(lsock, selectors.EVENT_READ, data=None)
+
+while True:
+    events = sel.select(timeout=None)
+    for key, mask in events:
+        if key.data is None:
+            accept_wrapper(key.fileobj)
+        else:
+            service_connection(key, mask)
